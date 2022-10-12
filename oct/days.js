8b8602405d9afb11ad398506311ebc49052433a4
NAMRATA <namratajhait@gmail.com>
2022-10-12 16:33:17 +0530

    my first commit

---
+++ oct/days.js
@@ -0,0 +1,6 @@
+function avg(day) {
+    
+    console.log(day === 1 ? 'Sunday' : day === 2 ? 'monday' : day === 3 ? 'Tuesday' : day === 4 ? 'Wednesday' : day === 5 ? 'Thursday' : day === 6 ? 'Friday' : day === 7 ? 'Saturday' : 'invaid')
+    
+}
+avg(100)
