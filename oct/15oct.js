be8bbacc304a56706da37ffd65677cd4767f70b7
NAMRATA <namratajhait@gmail.com>
2022-10-15 18:39:17 +0530

    my message commit

---
+++ oct/15oct.js
@@ -0,0 +1,8 @@
+function avg(){
+    if ( avg >= 18){
+        console.log('eligible')
+    }else {
+        console.log('not eligible')
+    }
+}
+avg(16)
---
+++ oct/get.js
@@ -0,0 +1,5 @@
+function operator(num){
+    console.log(num >= 18 || num <= 4 || "M                                                                                                                                              name is kajal" )
+}
+operator(16)
+
