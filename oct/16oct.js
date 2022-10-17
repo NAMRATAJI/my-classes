6683ec567a18fb9b73a5d3bca5537a9897117c96
NAMRATA <namratajhait@gmail.com>
2022-10-17 21:01:59 +0530

    commit my message

---
+++ oct/16oct.js
@@ -0,0 +1,57 @@
+//convert Km to miles
+function operator() {
+    const kilometers = 25;
+    // formula convert kilometers to miles 
+    const miles = kilometers * 0.621371
+    console.log(miles + "miles");
+}
+operator()
+// add two numbers
+function add() {
+var a = 10;
+var b = 900;
+var sum = a + b;
+console.log( + sum);
+}
+add()
+
+// check if a number is positive , negative , or zero
+function value(param) {
+    if (param < 0) {
+        console.log(" number is negative")
+    }
+    else if (param == 0) {
+        console.log("number is zero")
+   }
+    else{
+        console.log(" number is positive")
+    }
+    }
+    value(10)
+//check if a number is odd or even
+    function get(num) {
+        if (num % 2 == 0) {
+            console.log("this is even number")
+        }
+        else {
+            console.log("this is odd number")
+        }
+    }
+    get(9)
+//convert celsius to farenheit
+function cel(x, y, z) {
+    max_val = 0;
+    if (x > y)
+    {
+        max_val = x;
+    }
+    else{
+        max_val = y;
+    }
+    if (z > max_val)
+    {
+        max_val + z;
+    }return max_val;
+}
+cel(10,1,2)
+
