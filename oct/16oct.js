929db25779bd99d4d9723a2b101265cd449623d3
NAMRATA <namratajhait@gmail.com>
2022-10-20 16:06:44 +0530

    commit my message

--- oct/16oct.js
+++ oct/16oct.js
@@ -39,19 +39,5 @@ function value(param) {
     }
     get(9)
 //convert celsius to farenheit
-function cel(x, y, z) {
-    max_val = 0;
-    if (x > y)
-    {
-        max_val = x;
-    }
-    else{
-        max_val = y;
-    }
-    if (z > max_val)
-    {
-        max_val + z;
-    }return max_val;
-}
-cel(10,1,2)
+
 
---
+++ oct/20oct.js
@@ -0,0 +1,6 @@
+function it() {
+    for(let i = 0;i <= 100;i++){
+        console.log(i);
+    }
+}
+it()
